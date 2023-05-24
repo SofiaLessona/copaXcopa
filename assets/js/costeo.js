d223a2e370f61d7045476f9ff69328a952bfae12
Sofia Lessona <sofi.lessona@gmail.com>
2023-05-24 14:31:15 -0300

    pagina de costeo lista

--- assets/js/costeo.js
+++ assets/js/costeo.js
@@ -1,22 +1,27 @@
-let botonAgregar=document.querySelector("button");
-let contenedor=document.querySelector(".contenedor-costeo");
+let botonCalcular=document.querySelector(".boton-calcular");
+let mlPresentacion=document.querySelector("#mlPresentacion");
+let precioPresentacion=document.querySelector("#precioPresentacion");
+let mlReceta=document.querySelector("#mlReceta");
 
+let costo=document.querySelector("#costo");
 
-botonAgregar.addEventListener("click", (e)=>{
+botonCalcular.addEventListener("click", ()=>{
+    //Declaro variables y casteo los valores dados por usuario
+    let mlPres=parseFloat(mlPresentacion.value);
+    let mlRec=parseFloat(mlReceta.value);
+    let precioPres=parseFloat(precioPresentacion.value);
 
-    let formulario=document.createElement("form");
-    formulario.innerHTML=`
-        <label for="">Producto</label>
-        <input type="text" id="nombreProducto" placeholder="Nombre del producto"/>
+    //Calculo el costo de los ml del ingrediente
+    let precioRec=(mlRec*precioPres)/mlPres;
+    console.log("el precio calculado" + precioRec);
 
-        <label for="">Presentación</label>
-        <input type="text" placeholder="ml"/>
-        <input type="text" placeholder="$"/>
+    //Actualizo el valor de costo
+    costo.innerHTML=`
+        <h2>Costo de los ml de la receta:  $${precioRec.toFixed(2)}</h2>
+    `;
 
-        <label for="">Receta</label>
-        <input type="text" placeholder="ml"/>
-        <input type="text" placeholder="$"/>
-    `
-    console.log(formulario);
+});
 
-})
+const reiniciar=()=>{
+    location.reload();
+};
